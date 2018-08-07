748069b3e27d458a336b266d28e604b564d8a56b
Igor Lobo <lobo.igor19@gmail.com>
2018-08-07 13:20:06 -0300

    Exceção de posição e limpando o terminal

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,15 +1,41 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import xadrez.PartidaXadrez;
+import xadrez.PecaXadrez;
+import xadrez.PosicaoXadrez;
+import xadrez.XadrezExceptions;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		PartidaXadrez partida = new PartidaXadrez();
-		UI.imprimaTabuleiro(partida.getPecas());
-		
+		while(true) {
+		try {
+			UI.limparTela();
+			UI.imprimaTabuleiro(partida.getPecas());
+			System.out.println("\nPeça:");
+			PosicaoXadrez posInicial = UI.verificarPosicaoXadrez(sc);
+			
+			System.out.println("\nAlvo:");
+			PosicaoXadrez posFinal = UI.verificarPosicaoXadrez(sc);
+			
+			PecaXadrez pecaCapturada = partida.moverPeca(posInicial, posFinal);
+		}
+		catch(XadrezExceptions e){
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
+		catch(InputMismatchException e){
+			System.out.println(e.getMessage());
+			sc.nextLine();
+		}
 
+		
+		}
 	}
 
 }
